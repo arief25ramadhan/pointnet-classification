84327957e8445e001e1cdd2f61be819ee694d55a
arief <arief25ramadhan@yahoo.com>
2024-02-02 08:00:00 +0700

    Initialize scripts

--- model.py
+++ model.py
@@ -1,14 +1,11 @@
-
-def conv_bn(x, filters):
-    x = layers.Conv1D(filters, kernel_size=1, padding="valid")(x)
-    x = layers.BatchNormalization(momentum=0.0)(x)
-    return layers.Activation("relu")(x)
-
-
-def dense_bn(x, filters):
-    x = layers.Dense(filters)(x)
-    x = layers.BatchNormalization(momentum=0.0)(x)
-    return layers.Activation("relu")(x)
+import os
+import glob
+import trimesh
+import numpy as np
+from tensorflow import data as tf_data
+from keras import ops
+import keras
+from keras import layers
 
 
 class OrthogonalRegularizer(keras.regularizers.Regularizer):
@@ -24,43 +21,60 @@ class OrthogonalRegularizer(keras.regularizers.Regularizer):
         return ops.sum(self.l2reg * ops.square(xxt - self.eye))
 
 
-def tnet(inputs, num_features):
-    # Initalise bias as the indentity matrix
-    bias = keras.initializers.Constant(np.eye(num_features).flatten())
-    reg = OrthogonalRegularizer(num_features)
+class POINTNET_MODEL:
 
-    x = conv_bn(inputs, 32)
-    x = conv_bn(x, 64)
-    x = conv_bn(x, 512)
-    x = layers.GlobalMaxPooling1D()(x)
-    x = dense_bn(x, 256)
-    x = dense_bn(x, 128)
-    x = layers.Dense(
-        num_features * num_features,
-        kernel_initializer="zeros",
-        bias_initializer=bias,
-        activity_regularizer=reg,
-    )(x)
-    feat_T = layers.Reshape((num_features, num_features))(x)
-    # Apply affine transformation to input features
-    return layers.Dot(axes=(2, 1))([inputs, feat_T])
+    def __init__(self, num_points=2048, num_classes=10):
+        self.num_points = num_points
+        self.num_classes = num_classes
+    
+    def conv_bn(self, x, filters):
+        x = layers.Conv1D(filters, kernel_size=1, padding="valid")(x)
+        x = layers.BatchNormalization(momentum=0.0)(x)
+        return layers.Activation("relu")(x)
 
-inputs = keras.Input(shape=(NUM_POINTS, 3))
+    def dense_bn(self, x, filters):
+        x = layers.Dense(filters)(x)
+        x = layers.BatchNormalization(momentum=0.0)(x)
+        return layers.Activation("relu")(x)
 
-x = tnet(inputs, 3)
-x = conv_bn(x, 32)
-x = conv_bn(x, 32)
-x = tnet(x, 32)
-x = conv_bn(x, 32)
-x = conv_bn(x, 64)
-x = conv_bn(x, 512)
-x = layers.GlobalMaxPooling1D()(x)
-x = dense_bn(x, 256)
-x = layers.Dropout(0.3)(x)
-x = dense_bn(x, 128)
-x = layers.Dropout(0.3)(x)
+    def tnet(self, inputs, num_features):
+        # Initalise bias as the indentity matrix
+        bias = keras.initializers.Constant(np.eye(num_features).flatten())
+        reg = OrthogonalRegularizer(num_features)
 
-outputs = layers.Dense(NUM_CLASSES, activation="softmax")(x)
+        x = conv_bn(inputs, 32)
+        x = conv_bn(x, 64)
+        x = conv_bn(x, 512)
+        x = layers.GlobalMaxPooling1D()(x)
+        x = dense_bn(x, 256)
+        x = dense_bn(x, 128)
+        x = layers.Dense(
+            num_features * num_features,
+            kernel_initializer="zeros",
+            bias_initializer=bias,
+            activity_regularizer=reg,
+        )(x)
+        feat_T = layers.Reshape((num_features, num_features))(x)
+        # Apply affine transformation to input features
+        return layers.Dot(axes=(2, 1))([inputs, feat_T])
 
-model = keras.Model(inputs=inputs, outputs=outputs, name="pointnet")
-model.summary()
+    def get_model(self):
+
+        inputs = keras.Input(shape=(self.num_points, 3))
+        x = self.tnet(inputs, 3)
+        x = self.conv_bn(x, 32)
+        x = self.conv_bn(x, 32)
+        x = self.tnet(x, 32)
+        x = self.conv_bn(x, 32)
+        x = self.conv_bn(x, 64)
+        x = self.conv_bn(x, 512)
+        x = layers.GlobalMaxPooling1D()(x)
+        x = self.dense_bn(x, 256)
+        x = layers.Dropout(0.3)(x)
+        x = self.dense_bn(x, 128)
+        x = layers.Dropout(0.3)(x)
+        outputs = layers.Dense(self.num_classes, activation="softmax")(x)
+        model = keras.Model(inputs=inputs, outputs=outputs, name="pointnet")
+        # model.summary()
+
+        return model
--- train.py
+++ train.py
@@ -1,3 +1,22 @@
+import os
+import glob
+import trimesh
+import numpy as np
+from tensorflow import data as tf_data
+from keras import ops
+import keras
+from keras import layers
+from matplotlib import pyplot as plt
+
+# Load dataset
+data = POINTCLOUD_DATA()
+train_points, test_points, train_labels, test_labels, CLASS_MAP = data.parse_dataset()
+train_dataset, validation_dataset, test_dataset = data.get_dataset(train_points, test_points, train_labels, test_labels)
+
+# Load model
+pointnet = POINTNET_MODEL()
+model = pointnet.get_model()
+
 model.compile(
     loss="sparse_categorical_crossentropy",
     optimizer=keras.optimizers.Adam(learning_rate=0.001),
